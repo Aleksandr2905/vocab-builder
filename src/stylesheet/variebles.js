20a33a1b013c1a734f39f8a82dfd1d30b01b7235
Aleksandr2905 <natalukha@ukr.net>
2024-04-12 14:56:20 +0300

    Create variebles.js

---
+++ src/stylesheet/variebles.js
@@ -0,0 +1,28 @@
+export const variables = {
+  color: {
+    bgPrimary: "#F8F8F8",
+    bgSecondary: "#FFF",
+    textPrimary: "#121417",
+    textSecondary: "rgba(18, 20, 23, 0.80)",
+    accent: "#85AA9F",
+    accentLight: "#A5C0B8",
+    borderInput: "rgba(18, 20, 23, 0.10)",
+    error: "#D80027",
+    success: "#3CBF61",
+  },
+  fonts: {
+    regular: "MacPawFixelDisplay-Regular",
+    medium: "MacPawFixelDisplay-Medium",
+    semiBold: "MacPawFixelDisplay-SemiBold",
+    bold: "MacPawFixelDisplay-Bold",
+  },
+  breakpoints: {
+    mobileMin: "320px",
+    mobileMax: "375px",
+    tablet: "768px",
+    desktop: "1440px",
+  },
+  animation: {
+    transition: "all 0.3s ease",
+  },
+};
