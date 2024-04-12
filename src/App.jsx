e7dc5bbe23b336352096c023d9517e1295916a57
Aleksandr2905 <natalukha@ukr.net>
2024-04-12 15:45:53 +0300

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import DictionaryPage from "./pages/DictionaryPage/DictionaryPage";
+import RecommendPage from "./pages/RecommendPage/RecommendPage";
+import TrainingPage from "./pages/TrainingPage/TrainingPage";
+
+const App = () => {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <Routes>
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/dictionary" element={<DictionaryPage />} />
+      <Route path="/recommend" element={<RecommendPage />} />
+      <Route path="/training" element={<TrainingPage />} />
+    </Routes>
   );
-}
+};
 
 export default App;
